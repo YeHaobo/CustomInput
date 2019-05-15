0646f8755e0c640af759424e696b9ba54bdbc5bc
YeHaobo <17855366369@163.com>
2019-05-15 15:53:18 +0800

    持续更新

--- InputTest/app/src/main/java/com/yhb/inputtest/MainActivity.java
+++ InputTest/app/src/main/java/com/yhb/inputtest/MainActivity.java
@@ -13,19 +13,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.textInput = findViewById(R.id.easyInput);
-        /**
-         * setStartDrawable方法不可与setPwdDrawable方法并用，
-         * 若想在输入框头部添加图片
-         * 则可在XML中设置drawableStart、drawableLeft和drawablePadding属性
-         */
-        /**
-         * setPwdDrawable
-         * 该属性使用前必须设置密文输入不然无效果
-         */
+
+
         textInput.isShowLine(true)
                 .setLetterSpac(0.3f)
 //                .setStartDrawable(R.drawable.user,0)
+                /**
+                 * setStartDrawable方法不可与setPwdDrawable方法并用，
+                 * 若想在输入框头部添加图片
+                 * 则可在XML中设置drawableStart、drawableLeft和drawablePadding属性
+                 */
                 .setPwdDrawable(true)
+                /**
+                 * setPwdDrawable
+                 * 该属性使用前必须设置密文输入不然无效果
+                 */
                 .setHintText("是否上浮了？",true)
                 .setMaxText(10,true)
                 .setErrorListener(new IInputListener() {
